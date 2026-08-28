e3cc2561bfaf43e9d3030d9cfb08f804e3f80290
Mahmoud Radwan <mahmoudradwan11>
2026-08-28 03:57:37 +0300

    Setp Project

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,5 +1,7 @@
 class AppRoutes {
   static const splash = '/splash';
   static const onBoarding = '/onBoarding';
+  static const register = '/register';
+  static const login = '/login';
   static const home = '/';
 }
